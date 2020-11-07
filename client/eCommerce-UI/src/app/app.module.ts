24fe99ac6ce9f08eb0818fbac1da0ca5c5912bef
Yadav Dhakal <yadavdhkl@gmail.com>
2020-11-07 17:46:15 -0600

    Lazyloading

--- client/eCommerce-UI/src/app/app.module.ts
+++ client/eCommerce-UI/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
@@ -17,8 +16,7 @@ import { HomeModule } from './home/home.module';
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
-    ,HomeModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
