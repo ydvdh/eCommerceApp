e9e51200960d0f0e16dca9f3b339bbb618d13b26
Yadav Dhakal <yadavdhkl@gmail.com>
2020-11-09 15:49:33 -0600

    More on error handling using toastr

--- client/eCommerce-UI/src/app/core/interceptors/error.interceptor.ts
+++ client/eCommerce-UI/src/app/core/interceptors/error.interceptor.ts
@@ -1,22 +1,34 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private router: Router) { }
+    constructor(private router: Router, private toastrService: ToastrService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
             catchError(error => {
                 if (error) {
+                    if (error.status === 400) {
+                        if (error.error.errors) {
+                            throw error.error;
+                        } else {
+                        this.toastrService.error(error.error.message, error.error.statusCode);
+                        }
+                    }
+                    if (error.status === 401) {
+                        this.toastrService.error(error.error.message, error.error.statusCode);
+                    }
                     if (error.status === 404) {
                         this.router.navigateByUrl('/not-found');
                     }
                     if (error.status === 500) {
-                        this.router.navigateByUrl('/server-error');
+                        const navigationExtra: NavigationExtras = {state: {error: error.error}};
+                        this.router.navigateByUrl('/server-error', navigationExtra);
                     }
                 }
                 return throwError(error);
